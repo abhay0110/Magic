27f30d50ae2e56cfd33acd507a39a8422a4884b6
Abhay Pande <abhay0110@gmail.com>
2018-10-02 08:31:50 -0700

    CleanUp
    Updated and rearranged code. Removed old file.

--- script_new.js
+++ script_new.js
@@ -1,90 +1,106 @@
-var distToBottom, data, dataObj;
-var page = 1;
-var pollingForData = false;
-var xhr = new XMLHttpRequest();
-//var contentContainer = document.getElementsByClassName('content-container')[0];
-//var loadingContainer = document.getElementsByClassName('loading-container')[0];
-const app = document.getElementById('root');
+(function() {
+    var app, container, renderComplete = false,
+      page = 1,
+      xhr = new XMLHttpRequest();
 
+    function loadData(pageNum, pageSize) {
+      var url =
+        `https://api.magicthegathering.io/v1/cards?page=${pageNum}&pageSize=${pageSize}`;
+      xhr.open('GET', url, true);
+      xhr.send();
+    }
 
-const container = document.createElement('div');
-container.setAttribute('class', 'container');
-app.appendChild(container);
-function getDistFromBottom () {
+    function filterCards(cardData) {
+      var type = cardData.types,
+        image = cardData.imageUrl;
+      if (type.includes("Creature") && (image !== null || image !== ''))
+        return 1;
+      return 0;
+    }
 
-  var scrollPosition = window.pageYOffset;
-  var windowSize     = window.innerHeight;
-  var bodyHeight     = document.body.offsetHeight;
+    function renderCards(cardData) {
+      cardData.cards.forEach(function(cards, index) {
+        const card = document.createElement('div');
+        card.setAttribute('class', 'card');
+        const h2 = document.createElement('h2');
+        h2.textContent = cards.name;
 
-  return Math.max(bodyHeight - (scrollPosition + windowSize), 0);
+        var img = new Image();
+        const p = document.createElement('img');
+        p.src = cards.imageUrl;
+        img.setAttribute("class", "creature-img");
+        img.setAttribute("alt", "card image");
 
-}
+        const artist = document.createElement("p");
+        artist.textContent = `Artist: ${cards.artist}`;
+        //var x = document.createElement("LI");
+        const setName = document.createElement('p');
+        setName.textContent = `Set Name: ${cards.setName}`;
 
-xhr.onload = function() {
-  if(xhr.status === 200) {
-    pollingForData = false;
-    data = xhr.responseText
-    dataObj = JSON.parse(data);
-		console.log(dataObj);
+        const originalType = document.createElement("p");
+        originalType.textContent =
+          `Original Type: ${cards.originalType}`;
 
-    // for iterating through the data
-    // Using a ForEach
+        container.appendChild(card);
+        card.appendChild(h2);
+        card.appendChild(p);
+        card.appendChild(artist);
+        card.appendChild(setName);
+        card.appendChild(originalType);
+      })
+    }
 
-    dataObj.cards.forEach(function(cards, index){
-			const card = document.createElement('div');
-			card.setAttribute('class', 'card');
-			const h2 = document.createElement('h2');
-			h2.textContent = cards.name;
+    function initApp() {
+      app = document.getElementById('root');
+      container = document.createElement('div');
+      container.setAttribute('class', 'container');
+      app.appendChild(container);
 
-			var img = new Image();
-      const p = document.createElement('img');
-      p.src = cards.imageUrl;
-      img.setAttribute("class", "creature-img");
-      img.setAttribute("alt", "card image");
-
-      const artist = document.createElement("p");
-      artist.textContent = `Artist: ${cards.artist}`;
-      //var x = document.createElement("LI");
-      const setName = document.createElement('p');
-      setName.textContent = `Set Name: ${cards.setName}`;
-
-      const originalType = document.createElement("p");
-      originalType.textContent = `Original Type: ${cards.originalType}`;
-
-			container.appendChild(card);
-			card.appendChild(h2);
-			card.appendChild(p);
-			card.appendChild(artist);
-			card.appendChild(setName);
-			card.appendChild(originalType);
-    })
-
-//     // Using a For Loop
-//     for(var i = 0; i <= dataObj.posts.length; i++ ) {
-//       console.log('data ', i);
-//     }
-
-    // removing the spinner
-    // loadingContainer.classList.remove('no-content');
-
-  }
-};
-
-xhr.open('GET', 'https://api.magicthegathering.io/v1/cards?page=1&Page-Size=5', true);
-xhr.send();
-pollingForData = true;
-
-document.addEventListener('scroll', function() {
-        distToBottom = getDistFromBottom();
-         console.log('scrolling', getDistFromBottom());
-
-        if (!pollingForData && distToBottom > 0 && distToBottom <= 8888) {
-          pollingForData = true;
-          loadingContainer.classList.add('no-content');
-
-          page++;
-          xhr.open('GET', 'https://api.magicthegathering.io/v1/cards?page='+page+'&Page-Size=10', true);
-          xhr.send();
-
+      xhr.onload = function() {
+        if (xhr.status === 200) {
+          var filteredData = filterCards(JSON.parse(xhr.responseText));
+          // sorting the array as per name
+          filterreddata.sort(function(a, b) {
+              var nameA = a.name.toLowerCase(),
+                nameB = b.name.toLowerCase();
+              if (nameA < nameB) //sort string ascending
+                return -1;
+              if (nameA > nameB)
+                return 1;
+              return 0;
+            }
+            renderCards(filterreddata);
+            if (page === 1) {
+              renderComplete = true; //initial load complete, activate scroll handler
+            }
+          } else {
+            const errorMessage = document.createElement('p');
+            errorMessage.textContent = `Something is not working!`;
+            app.appendChild(errorMessage);
+          }
         }
-});
+
+        loadData(1, 10);
+      }
+
+      //app entry point
+      initApp();
+
+      //util method
+      function getDistFromBottom() {
+        var scrollPosition = window.pageYOffset,
+          windowSize = window.innerHeight,
+          bodyHeight = document.body.offsetHeight;
+
+        return Math.max(bodyHeight - (scrollPosition + windowSize), 0);
+      }
+
+      //check scroll position and load next page
+      document.addEventListener('scroll', function() {
+        if (renderComplete && getDistFromBottom() < 10) {
+          page++;
+          loadData(page, 20);
+        }
+      });
+
+    })();
